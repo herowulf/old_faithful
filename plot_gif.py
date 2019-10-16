ecfc17692ee15d5ca938b90344bdc390b7d7e0bc
Remco Volmer <rvolmer@valburg.science.ru.nl>
2019-10-16 09:48:24 +0200

    Added nice contour plots

--- plot_gif.py
+++ plot_gif.py
@@ -3,28 +3,43 @@ import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
 import matplotlib.animation as animation
+from scipy.stats import multivariate_normal
 
 
-def gif(data, group_list, mean_1, mean_2):
+def gif(data, group_list, mean_1, mean_2, cov_1, cov_2):
     Writer = animation.writers['ffmpeg']
     writer = Writer(fps=15, metadata=dict(artist='Me'), bitrate=1800)
 
     fig, ax = plt.subplots()
     fig.set_tight_layout(True)
 
+    x, y = np.mgrid[np.amin(data):np.amax(data):.01, np.amin(data):np.amax(data):.01]
+    pos = np.empty(x.shape + (2,))
+    pos[:, :, 0] = x; pos[:, :, 1] = y
+
+    rv_1 = multivariate_normal(mean_1[0], cov_1[0])
+    rv_2 = multivariate_normal(mean_2[0], cov_2[0])
+
+    cont1 = plt.contour(x, y, rv_1.pdf(pos), colors='r')
+    cont2 = plt.contour(x, y, rv_2.pdf(pos), colors='b')
     scat = plt.scatter(data[:,0], data[:,1], c=group_list[0])
-    scat_1 = plt.scatter(mean_1[0][0], mean_1[0][1], c='r')
-    scat_2 = plt.scatter(mean_2[0][0], mean_2[0][1], c='b')
 
 
     def update(i):
         label = 'timestep {0}'.format(i)
-        scat.set_array(group_list[i])
-        scat_1.set_offsets(mean_1[i])
-        scat_2.set_offsets(mean_2[i])
+        ax.clear()
+
+        rv_1 = multivariate_normal(mean_1[i], cov_1[i])
+        rv_2 = multivariate_normal(mean_2[i], cov_2[i])
+
+        cont1 = ax.contour(x, y, rv_1.pdf(pos), colors='r')
+        cont2 = ax.contour(x, y, rv_2.pdf(pos), colors='b')
+
+        scat = plt.scatter(data[:,0], data[:,1], c=group_list[i])
+
         ax.set_xlabel(label)
-        return scat, scat_1, scat_2, ax
+        return scat, cont1, cont2, ax
 
     anim = FuncAnimation(fig, update, frames=np.arange(0, len(group_list)), interval=500)
-    # anim.save('mooi.mp4', writer = writer)
+    anim.save('mooi.mp4', writer = writer)
     plt.show()
--- train.py
+++ train.py
@@ -15,6 +15,8 @@ data = (data - np.mean(data, axis=0))/np.std(data, axis=0)
 group = []
 mean_1 = []
 mean_2 = []
+cov_1 = []
+cov_2 = []
 
 group_1 = 0
 group_2 = 1
@@ -34,12 +36,12 @@ mean_2.append([1.6, -1.2])
 # mean_2.append(np.random.normal(size=2))
 
 # Initial covariance matrix
-cov_1 = np.array([[1,0],[0,1]])
-cov_2 = np.array([[1,0],[0,1]])
+cov_1.append(np.array([[1,0],[0,1]]))
+cov_2.append(np.array([[1,0],[0,1]]))
 
 # Calculate propability of each point belonging to group 1,2
-prob_1 = multivariate_normal.pdf(data, mean_1[-1], cov_1)
-prob_2 = multivariate_normal.pdf(data, mean_2[-1], cov_2)
+prob_1 = multivariate_normal.pdf(data, mean_1[-1], cov_1[-1])
+prob_2 = multivariate_normal.pdf(data, mean_2[-1], cov_2[-1])
 
 # Assign point to group dependingon where its probability is highest
 group.append(np.where(prob_1 > prob_2, group_1, group_2))
@@ -51,15 +53,15 @@ while len(group) < 2 or np.any(group[-1] != group[-2]):
     mean_2.append(np.mean(data[group[-1]==group_2], axis=0))
 
     # Calculate covariance matrix of points in each group
-    cov_1 = np.cov(data[group[-1]==group_1], rowvar=0)
-    cov_2 = np.cov(data[group[-1]==group_2], rowvar=0)
+    cov_1.append(np.cov(data[group[-1]==group_1], rowvar=0))
+    cov_2.append(np.cov(data[group[-1]==group_2], rowvar=0))
 
     # Calculate propability of each point belonging to group 1,2
-    prob_1 = multivariate_normal.pdf(data, mean_1[-1], cov_1)
-    prob_2 = multivariate_normal.pdf(data, mean_2[-1], cov_2)
+    prob_1 = multivariate_normal.pdf(data, mean_1[-1], cov_1[-1])
+    prob_2 = multivariate_normal.pdf(data, mean_2[-1], cov_2[-1])
 
     # Assign point to group dependingon where its probability is highest
     group.append(np.where(prob_1 > prob_2, group_1, group_2))
 
 # Make a gif of all the steps taken
-plot_gif.gif(data, group, mean_1, mean_2)
+plot_gif.gif(data, group, mean_1, mean_2, cov_1, cov_2)
